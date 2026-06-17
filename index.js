e07d965613e1d51504970cebbfa69a5e600a5e32
dulal71 <ahmeddulal4211@gmail.com>
2026-06-18 00:23:26 +0600

    connect to mongodb

--- index.js
+++ index.js
@@ -7,6 +7,30 @@ const port = 5000
 dotenv.config()
 app.use(cors())
 app.use(express.json())
+const uri=process.env.MONGODB_URI
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const database=client.db("blood-donation")
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
