adf1d1de11419aad70647115192ab61118b26d1e
dulal71 <ahmeddulal4211@gmail.com>
2026-06-18 00:13:55 +0600

    Initialize server project

---
+++ index.js
@@ -0,0 +1,18 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const dotenv=require('dotenv')
+const port = 5000
+dotenv.config()
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
