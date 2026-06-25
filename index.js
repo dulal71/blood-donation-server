97a77b1d26ecd4629ba44ed4ac45a9435d562186
dulal71 <ahmeddulal4211@gmail.com>
2026-06-25 19:16:01 +0600

    fixed mongodb connection issue

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const database=client.db("blood-donation")
     const donationRequestCollection=database.collection("donation-request")
    const userCollection = database.collection('user')
@@ -336,7 +336,7 @@ res.status(500).json({
   }
 })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
